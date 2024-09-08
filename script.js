d720cd92b7fc24d9fe52f019acab21e55abb6b28
joshhuyn <josh-it@outlook.com>
2024-09-08 22:27:28 +0200

    restructure

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.useless_window = {
     idDataMap : new Map(),
     currentId : "",
+    updateFunctions : [],
     colorutils : {
         hslToHex : (h, s, l) =>
         {
@@ -24,7 +25,8 @@ window.useless_window = {
         },
         hslToRgb : (h, s, l) =>
         {
-            return window.useless_window.colorutils.hslToHex(h, s, l);
+            let hex = window.useless_window.colorutils.hslToHex(h, s, l);
+            return window.useless_window.colorutils.hexToRgb(hex);
         },
         // color death convertion
         rgbToHsl : (r, g, b) => {
@@ -69,9 +71,8 @@ window.useless_window = {
             let canvasStyle = canvas.style;
             canvasStyle.width = "400px";
             canvasStyle.height = "200px";
-            canvasStyle.border = "solid black 2px";
 
-            updateFunc = () =>
+            function updateFunc()
             {
                 dataSet = window.useless_window.idDataMap.get(window.useless_window.currentId)
                 let ctx = canvas.getContext("2d");
@@ -83,7 +84,7 @@ window.useless_window = {
                         let hex = window.useless_window.colorutils.hslToHex(dataSet.hue, x, 100 - y);
                         ctx.fillStyle = hex;
                         ctx.strokeStyle = hex;
-                        ctx.strokeRect(x, y, x + 1, y + 1);
+                        ctx.strokeRect(x, y, 1, 1);
                     }
                 }
 
@@ -99,8 +100,65 @@ window.useless_window = {
                 dataSet.light = Math.floor((e.clientY - rect.top) / 2);
                 dataSet.saturation = Math.floor((e.clientX - rect.left) / 4);
                 
-                updateFunc();
+                window.useless_window.handler.repaint();
             });
+
+            return updateFunc;
+        },
+        addHueCanvas : div => {
+
+            let colorDispl = document.createElement("div");
+            colorDispl.style.width = "36px";
+            colorDispl.style.height = "36px";
+            colorDispl.style.margin = "2px";
+            colorDispl.style.border = "solid 1px #18122B";
+            colorDispl.style.borderRadius = "100%";
+
+            let canvas = document.createElement("canvas");
+            canvas.width = 358;
+            canvas.height = 1;
+            canvas.style.width = "358px";
+            canvas.style.height = "42px";
+
+            let wrapper = document.createElement("div");
+            wrapper.style.display = "flex";
+            wrapper.style.justifyContent = "center"
+            wrapper.style.alignItems = "center";
+
+            wrapper.appendChild(colorDispl);
+            wrapper.appendChild(canvas);
+            
+
+            div.appendChild(wrapper)
+
+            function updateFunc()
+            {
+                let dataSet = window.useless_window.idDataMap.get(window.useless_window.currentId)
+
+                let ctx = canvas.getContext("2d");
+                for (let i = 0; i <= 358; i++)
+                {
+                    let hex = window.useless_window.colorutils.hslToHex(i, 100, 50);
+                    ctx.fillStyle = hex;
+                    ctx.strokeStyle = hex;
+                    ctx.strokeRect(i, 0, i+1, 1);
+                }
+
+                ctx.fillStyle = "#000000";
+                ctx.strokeStyle = "#000000";
+                ctx.strokeRect(dataSet.hue, 0, 1, 1);
+
+                colorDispl.style.background = `hsl(${dataSet.hue}, ${dataSet.saturation}%, ${100 - dataSet.light}%)`;
+            }
+
+            canvas.addEventListener("mousedown", e => {
+                let dataSet = window.useless_window.idDataMap.get(window.useless_window.currentId)
+
+                let rect = canvas.getBoundingClientRect()
+                dataSet.hue = e.clientX - rect.left; 
+
+                window.useless_window.handler.repaint();
+            })
 
             return updateFunc;
         },
@@ -109,9 +167,17 @@ window.useless_window = {
             rgbDiv.style.display = "flex";
             rgbDiv.style.justifyContent = "center";
             rgbDiv.style.alignItems = "center";
-            rgbDiv.style.width = "300px";
+            rgbDiv.style.width = "400px";
             
             div.appendChild(rgbDiv);
+            
+            let textNode = document.createElement("div");
+            textNode.style.margin = "5px"
+            textNode.style.marginRight = "15px"
+            textNode.innerText = "RGB:";
+
+            rgbDiv.appendChild(textNode);
+
 
             let elements = []
             for (let i = 0; i < 3; i++)
@@ -119,19 +185,23 @@ window.useless_window = {
                 elements.push(document.createElement("input"))
             }
 
-            updateFunc = () =>
+            function updateFunc()
             {
                 dataSet = window.useless_window.idDataMap.get(window.useless_window.currentId);
                 let rgb = window.useless_window.colorutils.hslToRgb(dataSet.hue, dataSet.saturation, dataSet.light);
 
                 elements[0].value = rgb.r
-                elements[1].value = rgb.b
-                elements[2].value = rgb.g
+                elements[1].value = rgb.g
+                elements[2].value = rgb.b
             }
 
             elements.forEach(el=> {
-                el.type = "number";
+                el.type = "text";
                 el.style.width = "33%";
+                el.style.backgroundColor = "#443C68";
+                el.style.border = "none";
+                el.style.color = "#FFFFFF";
+                el.classList.add("color_picker_input")
                 rgbDiv.append(el);
 
                 el.addEventListener("blur", () => {
@@ -141,9 +211,9 @@ window.useless_window = {
                     dataSet.hue = hsl[0]
                     dataSet.saturation = hsl[1]
                     dataSet.light = hsl[2]
-                })
 
-                updateFunc();
+                    window.useless_window.handler.repaint();
+                })
             })
 
             return updateFunc;
@@ -153,27 +223,26 @@ window.useless_window = {
             div.id = "testInput";
 
             let divStyle = div.style;
-            divStyle.visibility = "visible";
+            divStyle.visibility = "hidden";
             divStyle.position = "absolute";
             divStyle.top = 0 + "px";
             divStyle.left = 0 + "px";
             divStyle.padding = "10px";
-            divStyle.backgroundColor = "yellow";
+            divStyle.backgroundColor = "#393053";
             divStyle.display = "flex";
             divStyle.justifyContent = "center";
             divStyle.alignItems = "center";
             divStyle.flexDirection = "column";
+            divStyle.border = "solid 1px #18122B";
+            divStyle.borderRadius = "5px";
 
-            updateFunction = []
+            updateFunction = window.useless_window.updateFunctions;
 
             updateFunction.push(window.useless_window.factory.addColorCanvas(div));
+            updateFunction.push(window.useless_window.factory.addHueCanvas(div));
             updateFunction.push(window.useless_window.factory.addControlDiv(div));
 
-            updateFunction.forEach(f => f())
-
-            //this.updateRGB();
-            
-            console.log(div);
+            window.useless_window.handler.repaint();
 
             return div;
         },
@@ -183,21 +252,48 @@ window.useless_window = {
         init : e =>
         {
             window.useless_window.handler.addDialog()
-            return;
+
             for (const i of document.getElementsByTagName("input"))
             {
-                if (i.type != "text")
+                if (i.classList.contains("color_picker_input"))
                 {
                     continue;
                 }
-
-                console.log(i)
-                addDialog()
+                
+                window.useless_window.handler.addButton(i)
             }
+        },
+        addButton : (i) =>
+        {
+            let id = window.useless_window.handler.generateId()
+            let data = window.useless_window.handler.createData()
+
+            let rect = i.getBoundingClientRect()
+
+            console.log(rect)
+
+            data.left = rect.right;
+            data.top = rect.bottom;
+
+            let btn = document.createElement("div");
+
+            btn.style.position = "absolute"
+            btn.style.backgroundColor = "black";
+            btn.style.width = rect.height - 8 + "px";
+            btn.style.height = rect.height - 8 + "px";
+            btn.style.top = rect.bottom - rect.height + 4;
+            btn.style.left = rect.right - rect.height + 4;
+
+            document.body.appendChild(btn);
+
+
+        },
+        generateId : () =>
+        {
+            return "id" + Math.random().toString(16).slice(2)
         },
         addDialog : () =>
         {
-            console.log("test")
             window.useless_window.handler.createData("test")
             window.useless_window.currentId = "test"
 
@@ -206,234 +302,23 @@ window.useless_window = {
         },
         createData : id =>
         {
-            window.useless_window.idDataMap.set(id,
-            {
+            let data = {
                 hue : 0,
                 light : 0,
                 saturation : 0,
-            })
+                top : 0,
+                left : 0,
+            };
+
+            window.useless_window.idDataMap.set(id, data);
+
+            return data;
+        },
+        repaint : () =>
+        {
+            window.useless_window.updateFunctions.forEach(f => f())
         }
     }
-}
-
-function attach(input)
-{
-    class DialogFactory {
-        hue = 100;
-        light = 0;
-        saturation = 0;
-
-        hueCanvas = undefined;
-        colorCanvas = undefined;
-
-        btn = undefined;
-
-        baseInput = undefined;
-
-        inputR = undefined;
-        inputG = undefined;
-        inputB = undefined;
-
-        constructor(btn, input)
-        {
-            this.btn = btn;
-            this.baseInput = input;
-
-            this.inputR = document.createElement("input");
-            this.inputG = document.createElement("input");
-            this.inputB = document.createElement("input");
-        }
-
-        updateRGB = () =>
-        {
-            console.log(this.hue, this.light, this.saturation)
-            let hex = ColorUtils.hslToHex(this.hue, this.light, this.saturation);
-            let rgb = ColorUtils.hexToRgb(hex);
-            this.inputR.value = rgb.r;
-            this.inputG.value = rgb.g;
-            this.inputB.value = rgb.b;
-
-            this.baseInput.value = rgb.r + "" + rgb.g + "" + rgb.b;
-
-            this.btn.getElementsByTagName("div")[0].style.backgroundColor = hex;
-        }
-
-        repaintHueCanvas = () =>
-        {
-            let ctx = this.hueCanvas.getContext("2d");
-            for (let i = 0; i <= 358; i++)
-            {
-                let hex = ColorUtils.hslToHex(i, 100, 50);
-                ctx.fillStyle = hex;
-                ctx.strokeStyle = hex;
-                ctx.strokeRect(i, 0, i+1, 1);
-            }
-
-            ctx.fillStyle = "#000000";
-            ctx.strokeStyle = "#000000";
-            ctx.strokeRect(this.hue, 0, 1, 1);
-        }
-
-        createHueCanvas = () =>
-        {
-            let canvas = document.createElement("canvas");
-            canvas.width = 358;
-            canvas.height = 1;
-            canvas.style.width = "358px";
-            canvas.style.height = "20px";
-
-            this.hueCanvas = canvas;
-            this.repaintHueCanvas(canvas);
-
-            canvas.addEventListener("mousedown", e => {
-                let rect = canvas.getBoundingClientRect()
-                this.hue = e.clientX - rect.left; 
-                this.repaintColorCanvas();
-                this.repaintHueCanvas();
-
-                this.updateRGB();
-            })
-
-            return canvas;
-        }
-
-        createControlDiv = () =>
-        {
-            let controlDiv = document.createElement("div");
-            controlDiv.style.border = "solid green 2px";
-
-            let hueCanvas = this.createHueCanvas();
-            controlDiv.appendChild(hueCanvas);
-            
-            let rgbDiv = document.createElement("div");
-            rgbDiv.style.display = "flex";
-            rgbDiv.style.justifyContent = "center";
-            rgbDiv.style.alignItems = "center";
-            rgbDiv.style.width = "300px";
-
-            this.inputR.type = "number";
-            this.inputR.style.width = "33%";
-            rgbDiv.append(this.inputR);
-
-            this.inputG.type = "number";
-            this.inputG.style.width = "33%";
-            rgbDiv.append(this.inputG);
-
-            this.inputB.type = "number";
-            this.inputB.style.width = "33%";
-            rgbDiv.append(this.inputB);
-
-            controlDiv.appendChild(rgbDiv);
-
-            return controlDiv;
-        }
-
-        newInstance = (id, left, top) =>
-        {
-            let div = document.createElement("div");
-            div.id = id;
-
-            let divStyle = div.style;
-            divStyle.visibility = "hidden";
-            divStyle.position = "absolute";
-            divStyle.top = top + "px";
-            divStyle.left = left + "px";
-            divStyle.padding = "10px";
-            divStyle.backgroundColor = "yellow";
-            divStyle.display = "flex";
-            divStyle.justifyContent = "center";
-            divStyle.alignItems = "center";
-            divStyle.flexDirection = "column";
-
-            let canvas = this.createColorCanvas();
-            div.appendChild(canvas);
-
-            let controlDiv = this.createControlDiv();
-            div.appendChild(controlDiv);
-
-            this.updateRGB();
-
-            return div;
-        }
-    }
-
-    class ColorUtils {
-        static hslToHex = (h, s, l) =>
-        {
-          l /= 100;
-          const a = s * Math.min(l, 1 - l) / 100;
-          const f = n => {
-            const k = (n + h / 30) % 12;
-            const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-            return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
-          };
-          return `#${f(0)}${f(8)}${f(4)}`;
-        }
-
-        static hexToRgb = (hex) =>
-        {
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-            } : null;
-        }
-
-        static hslToRgb = (h, s, l) =>
-        {
-            return this.hexToRgb(this.hslToHex(h, s, l));
-        }
-    }
-
-    let rect = input.getBoundingClientRect();
-
-    const id = "color_picker_something_" + Math.floor(Math.random() * 100);
-
-    let left = rect.left + rect.width;
-    let top = rect.top + rect.height;
-
-    let btn = createBtn(id, left, top);
-    let factory = new DialogFactory(btn, input);
-
-    let div = factory.newInstance(id, left, top);
-
-    document.body.appendChild(div)
-    document.body.appendChild(btn)
-}
-
-function createBtn(id, left, top)
-{
-    let wrapper = document.createElement("div");
-    let btn = document.createElement("div");
-
-    btn.style.width = "10px";
-    btn.style.height = "10px";
-    btn.style.backgroundColor = "hsl(100, 100, 0)";
-
-    wrapper.style.padding = "2px";
-    wrapper.style.position = "absolute";
-    wrapper.style.left = left - 20 + "px";
-    wrapper.style.top = top - 20 + "px";
-    wrapper.style.border = "solid 1px black"
-
-    wrapper.appendChild(btn);
-
-    wrapper.addEventListener("click", () => {
-        console.log("click")
-        let colorPicker = document.getElementById(id);
-
-        if (colorPicker.style.visibility == "visible")
-        {
-            colorPicker.style.visibility = "hidden";
-        }
-        else
-        {
-            colorPicker.style.visibility = "visible";
-        }
-    });
-
-    return wrapper;
 }
 
 setTimeout(window.useless_window.handler.init, 1000)
